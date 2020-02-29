c4dd25ace2d02fd9e7fc65a35c87c8d8ba47a413
KoehlerT <tim.koehler.net@gmail.com>
2020-02-29 14:09:58 +0100

    Added Setup project for Testing and Calibrating

---
+++ Setup/Inc/motors.h
@@ -0,0 +1,11 @@
+#ifndef MOTORS_H
+#define MOTORS_H
+
+#include "stm32f1xx_hal.h"
+
+extern int16_t esc_1, esc_2, esc_3, esc_4;
+
+extern void init_motors(TIM_HandleTypeDef*);
+extern void set_motors(int16_t);
+
+#endif // !MOTORS_H
