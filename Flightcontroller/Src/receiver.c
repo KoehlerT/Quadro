de49691105e924e1d2c7fe5987133621bbd481ba
KoehlerT <tim.koehler.net@gmail.com>
2020-03-17 13:39:16 +0100

    Adapted receiver input

--- Flightcontroller/Src/receiver.c
+++ Flightcontroller/Src/receiver.c
@@ -14,13 +14,13 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef* htim)
 	if (htim->Instance == TIM2)
 	{
 		measured_time = htim->Instance->CCR1 - measured_time_start;
-		if (measured_time < 0)measured_time += 0xFFFF;
+		if (measured_time < 0)measured_time += 0xFFFFFFFF;
 		measured_time_start = htim->Instance->CCR1;
 		
 		if (measured_time > 3000)channel_select_counter = 0;
 		else
 		{
-			channel[channel_select_counter] = measured_time;
+			channel[channel_select_counter] = (int ) (measured_time * 1.58f);//??
 			channel_select_counter++;
 		}
 	}
